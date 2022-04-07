d2dd6cc24db23c960d7aa25c6148f214d1f5c188
John Barragan <95306826+jostbago@users.noreply.github.com>
2022-04-06 20:37:50 -0500

    Update tablasdemultiplicar.java

--- tablasdemultiplicar.java
+++ tablasdemultiplicar.java
@@ -2,15 +2,27 @@
 Versión:        1.0
 Fecha:          20220325
 Copyright:      John Barragán
-Descripción:    Pide un número (que debe estar entre 0 y 10) y mostrar 
-la tabla de multiplicar de dicho número.
+Descripción:    Pide un número (que debe estar entre 0 y 10) y 
+mostrar la tabla de multiplicar de dicho número.
 */
 
 package Varios;
 
 import javax.swing.JOptionPane;
 
-public class tablasdemultiplicar {
-    
+public class tablademultiplicar {
 
+    public static void main(String args[])
+{
+    string texto;
+    int numero;
+    
+    numero = Integer.parseInt(JOptionPane.showInputDialog("Digite el numero de la tabla que quiere."));
+
+    for (int i = 0; i < 10; i++)
+    {
+        texto += numero + " x " + i + " = " + numero * i + "\n";
+    }
+    JOptionPane.showMessageDialog(null, "La tabla de multiplicar del " + numero +" es: \n " + texto);
+}
 }
